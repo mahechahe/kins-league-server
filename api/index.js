5ed2c1101c354791b58d84b559f8ef4d7eaf5d2d
Estiven Mahecha henao <mahechahe@gmail.com>
2023-01-06 16:16:54 -0500

    add get teams for id

--- api/index.js
+++ api/index.js
@@ -40,6 +40,13 @@ app.get('/presidents/:id', (ctx) => {
     : ctx.json({ message: 'President not found' }, 404)
 })
 
+app.get('/teams/:id', (ctx) => {
+  const id = ctx.req.param('id')
+  const foundTeam = teams.find((team) => team.id === id)
+
+  return foundTeam ? ctx.json(foundTeam) : ctx.json({ message: 'Team not found' }, 404)
+})
+
 app.get('/teams', (ctx) => {
   return ctx.json(teams)
 })
